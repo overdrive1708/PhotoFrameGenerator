713316f7fa86919eeee37363c915631fc08d2ce1
overdrive1708 <52090022+overdrive1708@users.noreply.github.com>
2026-04-29 15:11:51 +0900

    Add UI

---
+++ src/PhotoFrameGenerator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Reflection;
+
+namespace PhotoFrameGenerator.ViewModels
+{
+    internal partial class MainWindowViewModel : ObservableObject
+    {
+        //--------------------------------------------------
+        // バインディングデータ
+        //--------------------------------------------------
+        /// <summary>
+        /// タイトル
+        /// </summary>
+        [ObservableProperty]
+        private string _title;
+
+        //--------------------------------------------------
+        // メソッド
+        //--------------------------------------------------
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            Assembly assm = Assembly.GetExecutingAssembly();
+            string version = assm.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "";
+
+            // バージョン情報を取得してタイトルに反映する
+            Title = $"PhotoFrameGenerator Ver.{version}";
+        }
+    }
+}
